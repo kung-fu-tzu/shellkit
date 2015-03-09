6bae2e873f4d8de02364f8bc246f5ec47ae89a26
Peter Leonov <gojpeg@gmail.com>
2015-03-10 01:43:54 +0300

    near the run

--- shellkit.rb
+++ shellkit.rb
@@ -18,21 +18,26 @@ require 'open3'
 
 
 class RemoteShell
-  class Capture
-    def initialize sh, run_id
+  class Capture < ::Array
+    # TODO: push each Run to the current Capture, then "jQuery" on the ary of Runs
+  end
+
+  class Run
+    attr_reader :id, :status
+    def initialize sh, id, status
       @sh = sh
-      @run_id = run_id
+      @id = id
+      @status = status
     end
     def stdout
-      'sh.file_read'
+      
     end
   end
 
   def initialize args, &block
     @ssh_args = args
     
-    @tmpdir = Dir.mktmpdir(self.class.name.gsub(/\W/,'-'))
-    p @tmpdir
+    @ssh_multiplexing = ['-S', "#{local_tmpdir}/ssh-%r@%h:%p"]
     
     # -M for master connection
     @in, @out, @err, @wait_thread = Open3.popen3(*ssh_command, '-M', 'bash -')
@@ -41,32 +46,54 @@ class RemoteShell
     # Thread.new do
     #   @out.each_char { |char| print char }
     # end
-    @capture_stack = []
-    @capture_n = 0
+    
     as_dsl block if block
   end
-  
+
+  def local_tmpdir
+    @local_tmpdir ||= Dir.mktmpdir(self.class.name.gsub(/\W/,'-'), '/tmp/')
+  end
+
   def as_dsl block
     instance_exec &block
     close
   end
 
   def capture
-    @capture_stack.push Capture.new(self, @capture_n += 1)
+    @capture_stack ||= []
+    @capture_stack.push Capture.new
     yield
     @capture_stack.pop
   end
 
-  def remote_source_file_path
-    '/tmp/remote_shell_source_file'
+  def next_run_id
+    @run_id ||= 0
+    @run_id += 1
   end
   def run cmd
-    file_write remote_source_file_path, cmd
-    @in.write ". #{remote_source_file_path} </dev/null >/dev/null 2>&1; echo $?\n"
-    @status = @out.readline.to_i
-    return @status == 0
+    run_id = next_run_id
+    prefix = "#{tmpdir}/run-#{run_id}"
+    file_write "#{prefix}-cmd", cmd
+    @in.write ". #{prefix}-cmd </dev/null >#{prefix}-out 2>#{prefix}-err; echo $?\n"
+    status = @out.readline.chomp
+    status = status == '' ? nil : status.to_i
+    return Run.new(self, run_id, status)
   end
 
+  def tmpdir
+    @tmpdir ||= mktemp
+  end
+
+  def mktemp
+    i, o, e, w = Open3.popen3(*ssh_command, "mktemp -d")
+    i.close
+    e.close
+    name = o.read.chomp
+    status = w.value.exitstatus
+    raise "failed to open ssh connection" if status == 255
+    raise "failed to mktemp with status '#{status}'" unless status == 0
+    name
+  end
   def file_write path, data
     i, o, e, w = Open3.popen3(*ssh_command, "cat >#{path}")
     o.close
@@ -83,19 +110,24 @@ class RemoteShell
     io.close
     return data
   end
+  def read_file_read path
+    io = IO.popen([*ssh_command, "cat #{path}"], 'r')
+    data = io.read
+    io.close
+    return data
+  end
 
   def close
     @in.close
     @out.close
     @err.close
     @wait_thread.value
-    FileUtils.remove_entry @tmpdir
+    FileUtils.remove_entry local_tmpdir
   end
   
   def ssh_command
     # http://en.wikibooks.org/wiki/OpenSSH/Cookbook/Multiplexing
-    multiplexing = %w{-S /tmp/%r@%h:%p}
-    ['ssh', *multiplexing, *@ssh_args]
+    ['ssh', *@ssh_multiplexing, *@ssh_args]
   end
 end
 
